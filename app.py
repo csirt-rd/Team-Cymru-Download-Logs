f05f3f30f6ac4c6fc19512348390c1daef3d57e5
Oscar Encarnacion Liz <87453279+oencarnacion@users.noreply.github.com>
2022-04-24 22:23:03 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from urllib import response
+from bs4 import BeautifulSoup as bs
+import requests
+
+domain = 'https://cymru.com/$certname/'
+url = 'https://cymru.com/$certname/'
+username = "User Here"
+password = "Pass Here"
+filetype = '.txt'
+
+def get_soup(url):
+    return bs(requests.get(url, auth=(username, password)).text, 'html.parser')
+
+for link in get_soup(url).find_all('a'):
+    file_link = link.get('href')
+    if filetype in file_link:
+        print(file_link)
+        with open(link.text, 'wb') as file:
+            response = requests.get(domain + file_link, auth=(username, password))
+            file.write(response.content)
+
+print('Download complete!')
